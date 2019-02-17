cb26b87535021b66014bf5c801b50f009d613ec2
Kenneth Young Castro <kenneth.young90@gmail.com>
2019-02-17 04:05:53 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,6 +32,7 @@ class ApplicationController < Sinatra::Base
     @recipe.name = params[:name]
     @recipe.ingredients = params[:ingredients]
     @recipe.cook_time = params[:cook_time]
+    @recipe.save
     redirect "recipes/#{@recipe.id}"
   end
 
