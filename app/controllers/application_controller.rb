b1fc62212ee32418c942046cd103b0bf58dfcbcc
Kenneth Young Castro <kenneth.young90@gmail.com>
2019-02-17 03:37:39 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,8 +11,8 @@ class ApplicationController < Sinatra::Base
 
   post "/recipe" do
     recipe = Recipe.create(name: params[name],ingredients: params[ingredients], cook_time: params[cook_time])
-    if recipe 
-      recipe.save 
+    if recipe
+      recipe.save
       redirect "/recipe/#{recipe.id}"
   end
 
