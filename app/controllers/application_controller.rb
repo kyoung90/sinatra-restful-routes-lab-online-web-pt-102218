e6c014881c353c0bafa7336fa8b544f062adb4a2
Kenneth Young Castro <kenneth.young90@gmail.com>
2019-02-17 04:17:14 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < Sinatra::Base
   # code actions here!
   get "/recipes" do
     @recipes = Recipe.all
-    erb :all
+    erb :"recipes/all"
   end
 
   get "/recipes/new" do
